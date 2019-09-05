3e04c427d735e82a478b0bc891b9b63994c463cd
NingShen1997 <38674209+NingShen1997@users.noreply.github.com>
2019-09-05 10:51:02 -0700

    Exploration task on Sept 5th

---
+++ cm002-r_exploration.R
@@ -0,0 +1,37 @@
+number * 2
+number <- 3 + 4
+number * 2
+a * 2
+
+(times <- c(60, 40, 33, 15, 20, 55, 35))
+times / 60
+
+mean(times)
+sqrt(times)
+range(times)
+
+times < 30
+times == 20
+times != 20
+times > 20 & times < 50
+times > 20 | times < 50
+i = which(times == 20)
+
+times[times < 30]
+sum(times < 30)
+a <- all(times < 30)
+
+
+times[3]
+times[-3]
+times[c(2, 4)]
+times[c(4, 2)]
+times[1:5]
+times[times < 30]
+times
+times[times > 50] <- 50
+times
+
+
+
+
